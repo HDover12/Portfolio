e4aa5d0a365590bc1b6c51579525455c84e87b03
Hannah Dover <93832797+HDover12@users.noreply.github.com>
2021-12-18 22:09:24 -0500

    Add files via upload

--- FizzBuzz/fizzbuzz.js
+++ FizzBuzz/fizzbuzz.js
@@ -1,19 +1,24 @@
-let numbers = [];
 let input1;
 
 function fizzbuzz(input1) {
-  let numbers = [1];
+  let numbers = [];
   input1 = document.getElementById("input").value;
-  for (let i = 0; i < input1 - 1; i++) {
-    numbers.push(numbers[numbers.length - 1] + 1);
-    let new3Number = numbers[i] / 3;
-    let new5Number = numbers[i] / 5;
-    if (Number.isInteger(new3Number) && Number.isInteger(new5Number)) {
-      numbers[i] = "FizzBuzz";
-    } else if (Number.isInteger(new5Number)) {
-      numbers[i] = "Buzz";
-    } else if (Number.isInteger(new3Number)) {
-      numbers[i] = "Fizz";
+  Number(input1);
+  Math.floor(input1);
+  if (isNaN(input1) || input1 <= 0 || input1 > 100) {
+    alert("Please enter a number between 1 and 100");
+  } else {
+    for (let i = 0; i < input1; i++) {
+      numbers.push(i + 1);
+      let new3Number = numbers[i] / 3;
+      let new5Number = numbers[i] / 5;
+      if (Number.isInteger(new3Number) && Number.isInteger(new5Number)) {
+        numbers[i] = "FizzBuzz";
+      } else if (Number.isInteger(new5Number)) {
+        numbers[i] = "Buzz";
+      } else if (Number.isInteger(new3Number)) {
+        numbers[i] = "Fizz";
+      }
     }
   }
   document.getElementById("result").innerHTML = `[${numbers.join(",  ")}]`;
